1b3d940f6e226c6106060ed97dd84d6bea41e796
Ifechi-Maureen-Duru <maureen.duru@gordon.edu>
2022-11-09 15:47:16 -0800

    This is my first commit!

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Main from "./Components/Main";
+
+export default function App() {
+    const [darkMode, setDarkMode] = React.useState(true)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+    
+    return (
+        <div className="container">
+            <Navbar 
+                darkMode={darkMode} 
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode} />
+        </div>
+    )
+}
---
+++ src/Components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+export default function Main(props) {
+    return(
+        <main className={props.darkMode ? "dark" : ""}>
+        <h1 className="main-title">Fun Facts about React</h1>
+        <ul className="main-facts">
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+     </main>
+    )
+}
